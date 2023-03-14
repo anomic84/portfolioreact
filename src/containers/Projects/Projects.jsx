1b882a350714ee2504162ac599854a1c71aabcc5
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-13 21:27:20 -0700

    made links click to a new tab

--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
             </div>
             <p className="app__projects-card-info">{description}</p>
             <div className="">
-            <hr className="app__projects-card_hr" />
+              <hr className="app__projects-card_hr" />
               <div className="app__projects-card-title">Stack</div>
               <ul className="app__projects-card_footer-taglist">
                 {tags.map((tag, i) => (
@@ -26,8 +26,8 @@ const Projects = () => {
               </ul>
             </div>
             <ul className="app__projects-card_utility-list">
-              <li><a className="app__projects-card_utility-list_code" href={source}>Code</a></li>
-              <li><a className="app__projects-card_utility-list_code" href={visit}> Visit</a></li>
+              <li><a className="app__projects-card_utility-list_code" href={source} target="_blank" rel="noreferrer">Code</a></li>
+              <li><a className="app__projects-card_utility-list_code" href={visit} target="_blank" rel="noreferrer"> Visit</a></li>
             </ul>
           </div>
         ))}
