7ee12c254fad14f9f8d21f93980b5796d4ae8563
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-13 17:03:52 -0700

    added projexts and some minor fixes to other areas

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="app__header section__padding" id="home">
       <div className="app__header-content">
         <h1 className="gradient__text">Curious and Creative</h1>
-        <p>spencerccarlson@gmail.com</p>
+        <p>spencerccarlson0@gmail.com</p>
       </div>
       <div className="app__header-image">
         <img src={me} alt="Spencer" />
--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './projects.css'
+import { projects } from '../../constants/constants';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="app__projects gradient_bg2">
+      <h2 className="app__projects-title">Projects</h2>
+      <section className="app__projects-grid">
+        {projects.map(({ id, image, imagealt, title, description, tags, source, visit }) => (
+          <div className="app__projects-card" key={id}>
+            <img src={image} alt={imagealt} />
+            <div className="app__projects-card-title_area">
+              <h3 className="app__projects-card-title">{title}</h3>
+              <hr className="app__projects-card_hr" />
+            </div>
+            <p className="app__projects-card-info">{description}</p>
+            <div className="">
+            <hr className="app__projects-card_hr" />
+              <div className="app__projects-card-title">Stack</div>
+              <ul className="app__projects-card_footer-taglist">
+                {tags.map((tag, i) => (
+                  <li className="app__projects-card_footer-tag" key={i}>{tag}</li>
+                ))}
+              </ul>
+            </div>
+            <ul className="app__projects-card_utility-list">
+              <li><a className="app__projects-card_utility-list_code" href={source}>Code</a></li>
+              <li><a className="app__projects-card_utility-list_code" href={visit}> Visit</a></li>
+            </ul>
+          </div>
+        ))}
+      </section>
+    </section>
   )
 }
 
