0d0e14e21e2713d1600671e083539e78a4b5a21e
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-13 22:25:34 -0700

    messing with media queries

--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -1,35 +1,16 @@
 import React from 'react'
 import './projects.css'
 import { projects } from '../../constants/constants';
-
+import ProjectCard from '../../components/ProjectCard';
 const Projects = () => {
   return (
     <section className="app__projects gradient_bg2" id="projects">
       <div className="app__projects-divider" />
       <h2 className="app__projects-title">Projects</h2>
       <section className="app__projects-grid">
-        {projects.map(({ id, image, imagealt, title, description, tags, source, visit }) => (
-          <div className="app__projects-card" key={id}>
-            <img src={image} alt={imagealt} />
-            <div className="app__projects-card-title_area">
-              <h3 className="app__projects-card-title">{title}</h3>
-              <hr className="app__projects-card_hr" />
-            </div>
-            <p className="app__projects-card-info">{description}</p>
-            <div className="">
-              <hr className="app__projects-card_hr" />
-              <div className="app__projects-card-title">Stack</div>
-              <ul className="app__projects-card_footer-taglist">
-                {tags.map((tag, i) => (
-                  <li className="app__projects-card_footer-tag" key={i}>{tag}</li>
-                ))}
-              </ul>
-            </div>
-            <ul className="app__projects-card_utility-list">
-              <li><a className="app__projects-card_utility-list_code" href={source} target="_blank" rel="noreferrer">Code</a></li>
-              <li><a className="app__projects-card_utility-list_code" href={visit} target="_blank" rel="noreferrer"> Visit</a></li>
-            </ul>
-          </div>
+        
+        {projects.map((project) => (
+          <ProjectCard key={project.id} {...project}/>
         ))}
       </section>
     </section>
