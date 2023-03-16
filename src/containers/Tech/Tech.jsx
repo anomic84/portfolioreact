8084fb0bf55ab340d4b8a0780b3fdd2236d0ef3c
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-15 19:58:02 -0700

    i think i fixed it but dev tools lies a lot so we'll see

--- src/containers/Tech/Tech.jsx
+++ src/containers/Tech/Tech.jsx
@@ -8,29 +8,29 @@ const Tech = () => {
       <div className="app__tech-divider" />
       <br />
       <h2 className="app__tech-title">Technologies</h2>
-      <ul className="app__tech-list_area">
-        <li className="app__tech-list-item">
+      <div className="app__tech-list_area">
+        <div className="app__tech-list-item">
           <DiReact size="3rem" color="var(--color-red)" />
           <div className="app__tech-list-item_container">
             <h4 className="app__tech-list-item_title">Front-End</h4>
             <p className="app__tech-list-item_paragraph">Experience with <br />React, Next, and <br />Vite</p>
           </div>
-        </li>
-        <li className="app__tech-list-item">
+        </div>
+        <div className="app__tech-list-item">
           <DiNpm size="3rem" color="var(--color-red)" />
           <div className="app__tech-list-item_container">
             <h4 className="app__tech-list-item_title">Back-End</h4>
             <p className="app__tech-list-item_paragraph">Experience with <br />Node, MySql2, Sequelize,<br /> and MongoDB</p>
           </div>
-        </li>
-        <li className="app__tech-list-item">
+        </div>
+        <div className="app__tech-list-item">
           <DiSass size="3rem" color="var(--color-red)" />
           <div className="app__tech-list-item_container">
             <h4 className="app__tech-list-item_title">Design</h4>
             <p className="app__tech-list-item_paragraph">Experience with <br />Tailwind, SCSS, and <br />Styled-Components</p>
           </div>
-        </li>
-      </ul>
+        </div>
+      </div>
     </section >
   )
 }
